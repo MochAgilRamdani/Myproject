a5136bf1c8d042065ae1a519d696ef434b76134e
MochAgilRamdani <127289005+MochAgilRamdani@users.noreply.github.com>
2023-06-22 13:34:01 +0700

    Add files via upload

---
+++ biodata_Agil.py
@@ -0,0 +1,15 @@
+print ("=======================================")
+print ("Program Biodata Sederhana Dengan Python")
+print ("=======================================")
+
+nama = "Moch Agil Ramdani"
+hobi = "bermain game,futsal"
+alamat = "Maleber,kalibunder regency blok g23"
+Kota = "Cianjur"
+Propinsi = "Jawa Barat"
+
+print("Nama Lengkap  :",nama)
+print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
+print("Hobi          :", hobi)
+print ("Selamat Datang di",nama )
+print ("=======================================")
